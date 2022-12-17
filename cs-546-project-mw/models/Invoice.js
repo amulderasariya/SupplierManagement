83aacf8cf2e6fef07bceac83368a6a8b0176208a
Yash Panchal <yapster1999@gmail.com>
2022-12-17 00:55:30 -0500

    ownerID key changed

--- cs-546-project-mw/models/Invoice.js
+++ cs-546-project-mw/models/Invoice.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const invoiceSchema = new Schema({
-  ownerId: {
+  ownerID: {
     type: String,
     required: true,
     trim: true,
